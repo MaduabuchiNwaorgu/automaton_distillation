1e6651a24e21cb0d394e88fdbb58ec0730674cab
surajsingireddy <surajsingireddy@hotmail.com>
2022-05-29 14:46:34 -0400

    added Dungeon Quest environment
    - added a new environment and run script for a teacher DQN

---
+++ automaton_transfer/run/teacher/dungeon_quest_teacher.py
@@ -0,0 +1,12 @@
+import torch
+
+from automaton_transfer.lib.main import run_training
+from automaton_transfer.run.env.dungeon_quest import dungeon_quest_env_config
+from automaton_transfer.run.utils import teacher_config_v1
+from automaton_transfer.run.env.dungeon_quest import dungeon_quest_aps, dungeon_quest_ltlf
+
+device = torch.device("cuda:0")
+config = teacher_config_v1(dungeon_quest_env_config, "dungeon_quest_teacher", device)
+
+if __name__ == '__main__':
+    run_training(config)
