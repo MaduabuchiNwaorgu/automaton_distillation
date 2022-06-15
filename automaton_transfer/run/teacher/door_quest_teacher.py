e88ff23d90401ff1e26d3bac83c646de43a6d61d
surajsingireddy <surajsingireddy@hotmail.com>
2022-06-15 10:04:42 -0400

    Door Quest + Product MDP
    - Added simpler environment, Door Quest. The agent only needs to find a key (positioned randomly on the grid) and then reach the door.
    - Added teacher run scripts for learning over product MDPs

---
+++ automaton_transfer/run/teacher/door_quest_teacher.py
@@ -0,0 +1,12 @@
+import torch
+
+from automaton_transfer.lib.main import run_training
+from automaton_transfer.run.env.door_quest import door_quest_env_config
+from automaton_transfer.run.utils import teacher_config_v1
+from automaton_transfer.run.env.door_quest import door_quest_aps, door_quest_ltlf
+
+device = torch.device("cuda:0")
+config = teacher_config_v1(dungeon_quest_env_config, "door_quest_teacher", device)
+
+if __name__ == '__main__':
+    run_training(config)
---
+++ automaton_transfer/run/teacher/dungeon_quest_teacher_rew_per_step_7_productMDP.py
@@ -0,0 +1,15 @@
+import torch
+
+from automaton_transfer.lib.agent.one_hot_automaton_agent import OneHotAutomatonAfterFeatureExtractorAgent
+from automaton_transfer.lib.main import run_training
+from automaton_transfer.run.env.dungeon_quest_7 import dungeon_quest_rew_per_step_env_config_7
+from automaton_transfer.run.env.dungeon_quest import dungeon_quest_aps, dungeon_quest_ltlf
+from automaton_transfer.run.utils import teacher_config_v1
+
+device = torch.device("cuda:0")
+config = teacher_config_v1(dungeon_quest_rew_per_step_env_config_7, "dungeon_quest_teacher_rew_per_step_7_productMDP",
+                           device, agent_cls=OneHotAutomatonAfterFeatureExtractorAgent, aps=dungeon_quest_aps,
+                           ltlf=dungeon_quest_ltlf)
+
+if __name__ == '__main__':
+    run_training(config)
